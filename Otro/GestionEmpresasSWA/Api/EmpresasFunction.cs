7a148e4350d2af5f7396ad687cbc3edcbda10f44
SFayosAlcaraz <s.fayosalcaraz@edu.gva.es>
2026-03-06 17:40:16 +0100

    otra

---
+++ Otro/GestionEmpresasSWA/Api/EmpresasFunction.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function;
+
+public class EmpresasFunction
+{
+    private readonly ILogger<EmpresasFunction> _logger;
+
+    public EmpresasFunction(ILogger<EmpresasFunction> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("EmpresasFunction")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
